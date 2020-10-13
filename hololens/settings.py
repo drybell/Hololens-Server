fe66d6c25766757ca84123eabe9392dda6e9caf8
Daniel Ryaboshapka <dan4821@gmail.com>
2020-10-13 16:02:20 -0400

    fix settings

--- hololens/settings.py
+++ hololens/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = './backend/templates/'
+STATIC_URL = '/static/'
+
