05106bf3652c4ade7bc19c092d3831dfe4d0d0c2
Daniel Ryaboshapka <dan4821@gmail.com>
2020-10-13 16:37:27 -0400

    fix cube

--- hololens/settings.py
+++ hololens/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = './backend/static/'
+STATIC_ROOT = '/backend/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # new
